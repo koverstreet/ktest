04f9b457d993d3bb924a845e21a638d54334de1d
jfr <jfr@datera.io>
2014-05-09 08:17:53 -0700

    Checking in modified vm-start (integrated with vm-cluster code)

---
+++ vmbrtmpl.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# ======================================================
+# Copyright (c) 2014 Datera, Inc.  All Rights Reserved.
+# Datera, Inc. Confidential and Proprietary Information.
+# ======================================================
+
+vmbr_ifdn = """#!/bin/bash
+set -x
+
+err="ok"
+if [ ! $(which brctl) ]; then
+  err="brctl"
+elif [ ! $(which ip) ]; then
+  err="ip"
+fi
+
+if [ "$err" != "ok" ]; then
+  echo "$0: Failed to remove tap interface - $err command not found"
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+  echo "Error: no interface specified"
+  exit 1
+fi
+
+brctl delif ${bridge} $1
+sleep 0.5s
+ip link set $1 down
+exit 0
+
+"""
+
+vmbr_ifup = """#!/bin/bash
+set -x
+
+err="ok"
+if [ ! $(which brctl) ]; then
+  err="brctl"
+elif [ ! $(which ip) ]; then
+  err="ip"
+elif [ ! $(which tunctl) ]; then
+  err="tunctl"
+fi
+
+if [ "$err" != "ok" ]; then
+  echo "$0: Failed to add tap interface - $err command not found"
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+  echo "Error: no interface specified"
+  exit 1
+fi
+
+tunctl -u `whoami` -t $1
+ip link set $1 up
+sleep 0.5s
+brctl addif ${bridge} $1
+exit 0
+
+"""
+
