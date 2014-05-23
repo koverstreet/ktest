e61456e31edfa0ef9387637cf4a363f93dd91ae2
Kent Overstreet <kmo@daterainc.com>
2014-05-23 16:58:03 -0700

    Change how dependencies are specified
    Change-Id: I3f57e2939508e3e58b83213eda1219884c81ada5

--- tests/test-libs.sh
+++ tests/test-libs.sh
@@ -445,20 +445,19 @@ test_mkfs_stress()
 	#test_powerfail
 }
 
-dmesg -n 7
-echo 1 > /proc/sys/kernel/sysrq
+#dmesg -n 7
+#echo 1 > /proc/sys/kernel/sysrq
 
-ln -sf /sys/kernel/debug/tracing/ /root/t || true
+#ln -sf /sys/kernel/debug/tracing/ /root/t || true
 
-set -x
+#set -x
 # mount /var/log device if it exists
-if [ -b /dev/disk/by-uuid/11111111-2222-3333-4444-555555555555 ] ; then
-    systemctl stop rsyslog
-    mount /dev/disk/by-uuid/11111111-2222-3333-4444-555555555555 /var/log
-    systemctl start rsyslog
-    mkdir -p /var/log/core
-    echo 1 > /proc/sys/fs/suid_dumpable
-    echo /var/log/core/core.%e.PID%p.SIG%s.TIME%t > /proc/sys/kernel/core_pattern
-    ulimit -c unlimited
-fi
-
+#if [ -b /dev/disk/by-uuid/11111111-2222-3333-4444-555555555555 ] ; then
+#    systemctl stop rsyslog
+#    mount /dev/disk/by-uuid/11111111-2222-3333-4444-555555555555 /var/log
+#    systemctl start rsyslog
+#    mkdir -p /var/log/core
+#    echo 1 > /proc/sys/fs/suid_dumpable
+#    echo /var/log/core/core.%e.PID%p.SIG%s.TIME%t > /proc/sys/kernel/core_pattern
+#    ulimit -c unlimited
+#fi
