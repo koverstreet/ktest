cb50bef3c4b037dc53dfc84cf56646160d640b97
Claudio Fleiner <cmf@daterainc.com>
2014-06-15 11:28:32 -0700

    move usage() so it gets printed when script is called with no args
    also enable shellcheck for ktest and util.sh, plus lots of fixes
    required to make it pass. (some actual bugs, style issues, etc)
    
    Change-Id: Iba7b685c35c65b18e1a86f5083a92de40dce9462

--- util.sh
+++ util.sh
@@ -9,13 +9,13 @@ checkdep()
 	    PACKAGE=$COMMAND
 	fi
 
-	if ! which $COMMAND > /dev/null; then
+	if ! which "$COMMAND" > /dev/null; then
 		echo -n "$COMMAND not found"
 
 		if which apt-get > /dev/null && \
 			which sudo > /dev/null; then
 			echo ", installing $PACKAGE:"
-			sudo apt-get install -y $PACKAGE
+			sudo apt-get install -y "$PACKAGE"
 		else
 			echo ", please install"
 			exit 1
