946b30857fefea32f3a7c9d923fb287e8ee7fbec
Slava Pestov <sp@daterainc.com>
2014-10-13 14:41:56 -0700

    Performance enhancement
    Change-Id: Id1b725beecd6e51d375b7d374751a461024500b8

--- tests/bcache/bcache-test-libs.sh
+++ tests/bcache/bcache-test-libs.sh
@@ -237,7 +237,7 @@ cache_set_settings()
 {
     for dir in $(ls -d /sys/fs/bcache/*-*-*); do
 	true
-	echo 1 > $dir/btree_scan_ratelimit
+	echo 0 > $dir/btree_scan_ratelimit
 
 	#echo 0 > $dir/synchronous
 	echo panic > $dir/errors
