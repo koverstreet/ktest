41e31bb1ef1bead3ab9a189308cb417229ff8c0a
Slava Pestov <sp@daterainc.com>
2015-01-21 20:53:46 -0800

    reboot_fs tests: test XFS if priority > 0
    Change-Id: I4d560c79a85917b721f32190c6ef05cfbb5fd804

--- tests/bcache/reboot-fs.sh
+++ tests/bcache/reboot-fs.sh
@@ -1,24 +1,41 @@
 config-cpus 1
 
-nr_iterations=$((($ktest_priority + 1) * 5))
+nr_iterations=$((($ktest_priority + 1) * 4))
 config-timeout $(($nr_iterations * 45 + $(stress_timeout)))
+
+# If test priority is > 0, also do XFS tests
+if [ $ktest_priority -gt 0 ]; then
+    N=$((nr_iterations / 2))
+else
+    N=$((nr_iterations + 1))
+fi
 
 main()
 {
     setup_tracing 'bcache:*'
 
-    if [ $NR_REBOOTS = 0 ]; then
+    # On the first N reboots, test ext4, then test xfs
+    if [ $NR_REBOOTS -lt $N ]; then
+	FS=ext4
+    else
+	FS=xfs
+    fi
+
+    # On the first reboot and the Nth reboot, re-format
+    # the file system
+    if [ $NR_REBOOTS = 0 -o $NR_REBOOTS = $N ]; then
 	setup_bcache
-	setup_fs ext4
+	setup_fs $FS
     else
 	existing_bcache
-	existing_fs ext4
+	existing_fs $FS
 	rm -rf /mnt/$dev/*
     fi
 
     test_antagonist
 
-    if [ $NR_REBOOTS = $nr_iterations ]; then
+    # Right before we end or switch to XFS, shut down bcache
+    if [ $NR_REBOOTS = $nr_iterations -o $NR_REBOOTS = $((N - 1)) ]; then
 	test_dbench
 	test_bonnie
 	stop_fs
@@ -29,6 +46,10 @@ main()
 	test_bonnie &
 
 	sleep 30
+    fi
+
+    # On any iteration but the last, reboot
+    if [ $NR_REBOOTS != $nr_iterations ]; then
 	do_reboot
     fi
 }
