2c04437d6d8ee38c254c2b683e838e4f30bb8d9c
Kent Overstreet <kent.overstreet@linux.dev>
2022-09-03 23:45:57 -0400

    ci: Redirect test output to /dev/null
    We get this in the dashboard: instead, let's have stdout for debugging
    the CI itself.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- ci/_test-git-branch.sh
+++ ci/_test-git-branch.sh
@@ -84,7 +84,7 @@ while (( ${#SUBTESTS[@]} )); do
 
     $KTEST_DIR/lib/supervisor -T 1200 -f "$FULL_LOG" -S -F	\
 	-b $TEST_NAME -o ktest-out/out				\
-	-- build-test-kernel run $TEST_PATH ${SUBTESTS[@]} &
+	-- build-test-kernel run $TEST_PATH ${SUBTESTS[@]} > /dev/null &
     wait
 
     SUBTESTS_REMAINING=()
