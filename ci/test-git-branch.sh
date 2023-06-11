45269422cb97ba03d6cb2690fffd7c414f071240
Kent Overstreet <kent.overstreet@linux.dev>
2023-06-10 21:36:47 -0400

    test-git/branch: Handle git fetch errors
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- ci/test-git-branch.sh
+++ ci/test-git-branch.sh
@@ -42,6 +42,7 @@ git_fetch()
 	    git fetch ssh://$JOBSERVER/$JOBSERVER_HOME/$repo $@
 	    ret=$?
 	    (($ret == 0)) && break
+	    (($ret == 128)) && exit 1
 	    echo "git fetch returned $ret"
 	    sleep 1
 	done
