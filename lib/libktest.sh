7d02981300c6c52bf9e087340109784d6ebe9acd
Janpieter Sollie <janpieter.sollie@edpnet.be>
2023-09-22 21:17:33 +0200

    enable ARMv7 support
    Ktest has now been expanded with cross-binary armv7 (hard float).
    this should allow some testing on more legacy armv7 hardware,
    which may still be present in NAS devices.
    However, due to the nature of the ecosystem, a limit is in place:
    - 3GB ram for 32 bit systems
    - 4 CPUs for 32 bit systems
    I think if you need more, you'd better think of going 64-bit.
    These constraints allow the ARM emulator to run smoothly.
    
    Signed-off-by:  jpsollie <janpieter.sollie@edpnet.be>

--- lib/libktest.sh
+++ lib/libktest.sh
@@ -223,7 +223,7 @@ ktest_kgdb()
 
 ktest_mon()
 {
-    exec socat UNIX-CONNECT:"$ktest_out/vm/on" STDIO
+    exec socat UNIX-CONNECT:"$ktest_out/vm/mon" STDIO
     exec nc "$ktest_out/vm/mon"
 }
 
@@ -314,7 +314,7 @@ start_vm()
 	x86|x86_64)
 	    qemu_cmd+=(-cpu $cputype -machine type=q35,accel=$accel,nvdimm=on)
 	    ;;
-	aarch64)
+	aarch64|arm)
 	    qemu_cmd+=(-cpu $cputype -machine type=virt,gic-version=max,accel=$accel)
 	    ;;
 	mips)
@@ -327,9 +327,12 @@ start_vm()
     esac
 
     local maxmem=$(awk '/MemTotal/ {printf "%dG\n", $2/1024/1024}' /proc/meminfo 2>/dev/null) || maxmem="1T"
+    local memconfig="$ktest_mem,slots=8,maxmem=$maxmem"
+
+    [ $BITS == 32 ] &&  memconfig="3G" && ktest_cpus=4 #do not be fancy on 32-bit hardware.  if it works, it's fine
 
     qemu_cmd+=(								\
-	-m		"$ktest_mem,slots=8,maxmem=$maxmem"		\
+	-m		"$memconfig"					\
 	-smp		"$ktest_cpus"					\
 	-kernel		"$ktest_kernel_binary/vmlinuz"			\
 	-append		"$(join_by " " ${kernelargs[@]})"		\
@@ -342,6 +345,8 @@ start_vm()
 	-device		virtio-rng-pci					\
 	-virtfs		local,path=/,mount_tag=host,security_model=none,multidevs=remap	\
     )
+
+
 
     if [[ -f $ktest_kernel_binary/initramfs ]]; then
 	qemu_cmd+=(-initrd 	"$ktest_kernel_binary/initramfs")
