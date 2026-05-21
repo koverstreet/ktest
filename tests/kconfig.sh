7cf700f5101914040b8117a304e7ea8ece650dea
Kent Overstreet <kent.overstreet@linux.dev>
2026-05-21 17:45:07 -0400

    can't enable CONFIG_RUST until 7.0

--- tests/kconfig.sh
+++ tests/kconfig.sh
@@ -343,7 +343,3 @@ require-kernel-config FAULT_INJECTION_CONFIGFS
 # Nix:
 require-kernel-config SECCOMP
 require-kernel-config SECCOMP_FILTER
-
-# Rust:
-require-kernel-config RUST
-config-compiler clang
