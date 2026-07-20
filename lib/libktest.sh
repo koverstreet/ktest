koverstreet/ktest

commit 88f1e27b79118fbd6b8ee93416c23814d01090ed

start_vm: handle > 24 block devices

lib/libktest.sh
@@ -468,6 +468,8 @@ start_vm()
 
     qemu_cmd+=(								\
 	-m		"$ktest_mem,slots=8,maxmem=$maxmem"		\
+	-device pcie-root-port,id=pci.1,bus=pcie.0,slot=1,chassis=1	\
+	-device pcie-pci-bridge,id=pci.2,bus=pci.1			\
 	-smp		"$ktest_cpus"					\
 	-kernel		"$ktest_kernel_binary/vmlinuz"			\
 	-append		"$(join_by " " ${kernelargs[@]})"		\
@@ -545,7 +547,11 @@ start_vm()
 		qemu_cmd+=(-device ide-hd,bus=hba.$disknr,drive=disk$disknr)
 		;;
 	    virtio-blk)
-		qemu_cmd+=(-device virtio-blk-pci,drive=disk$disknr)
+		if [[ $disknr < 20 ]]; then
+		    qemu_cmd+=(-device virtio-blk-pci,drive=disk$disknr)
+		else
+		    qemu_cmd+=(-device virtio-blk-pci,drive=disk$disknr,bus=pci.2)
+		fi
 		;;
 	    *)
 		qemu_cmd+=(-device scsi-hd,bus=hba.0,drive=disk$disknr)

commit edeadcf88d4b130b7eb6e0ba69bc3f6cf1cf3217

libktest: fix lexicographic disk-number comparison in virtio-blk routing
[[ $disknr < 20 ]] compares strings: "3" < "20" is false ('3' > '2'),
so disks 3-9 were routed to the pci.2 overflow bridge meant for disk
20+. On the arm64 runners qemu auto-assigns from slot 0 on that bridge,
which the standard hotplug controller rejects - any test with four or
more scratch devices died before boot ("Unsupported PCI slot 0",
seen on ec.ktest on farm4). Use an arithmetic comparison.

Fixes: 88f1e27b7911 ("start_vm: handle > 24 block devices")

Co-Authored-By: Proof of Concept <poc@bcachefs.org>

lib/libktest.sh
@@ -547,7 +547,7 @@ start_vm()
 		qemu_cmd+=(-device ide-hd,bus=hba.$disknr,drive=disk$disknr)
 		;;
 	    virtio-blk)
-		if [[ $disknr < 20 ]]; then
+		if (( disknr < 20 )); then
 		    qemu_cmd+=(-device virtio-blk-pci,drive=disk$disknr)
 		else
 		    qemu_cmd+=(-device virtio-blk-pci,drive=disk$disknr,bus=pci.2)